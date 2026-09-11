a437bf116c6159d650cf317ca6cdc5a7afa4d323
WoBuWanGiTeHaBu <3154752340@qq.com>
2026-09-12 01:38:20 +0800

    Initial commit

--- src/main/java/com/theos/detectivehelper/DetectiveHelperApplication.java
+++ src/main/java/com/theos/detectivehelper/DetectiveHelperApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DetectiveHelperApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DetectiveHelperApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DetectiveHelperApplication.class, args);
+    }
 
 }
